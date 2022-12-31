01507c227a10ae1921bfaecbe87080065d8958d2
mb1503 <miguel.bay200315@gmail.com>
2022-12-30 23:38:59 -0500

    Added to path class
    Do we need attackBehavior if it uses the same condition as path function? Hmm

--- src/goblin.py
+++ src/goblin.py
@@ -28,13 +28,14 @@ class Goblin(enemy.Enemy):
         if(self.sqrDistanceToPlayer(self, plr) < range*range): # range is a variable u declare at the start, represents the pixel range that the player needs to be within for the enemy to start chasing the player
             self.gotoX = plr.x
             self.gotoY = plr.y
+            currentSprite = 1;
         else:
             self.gotoX = self.pathToPoints[0].x
             self.gotoY = self.pathToPoints[0].y
             #remove path to points [0] once they have reached it
 
     def attackBehavior(self):
-        currentSprite = 1;
+        pass;
 
     def draw(self, surf):
         super().draw()
