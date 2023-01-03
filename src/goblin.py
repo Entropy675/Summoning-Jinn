9484365565c3300950c5cbb48560847205e4febc
mb1503 <miguel.bay200315@gmail.com>
2023-01-03 00:28:29 -0500

    Updated attackBehavior and update
    update should be fine, attackBehavior seems right, unless I need to change something with the x y coordinates but the attacks should be in the currentSprite

--- src/goblin.py
+++ src/goblin.py
@@ -4,10 +4,13 @@ import math
 import basicSprite
 import level
 import entity
+import enemy
 import player
 
 
 class Goblin(enemy.Enemy):
+
+    range = 16 #range for when goblin will follow player
 
     currentSprite = 0;
     sprites = []; # basicSprite
@@ -25,17 +28,27 @@ class Goblin(enemy.Enemy):
 
     def path(self):
 
-        if(self.sqrDistanceToPlayer(self, plr) < range*range): # range is a variable u declare at the start, represents the pixel range that the player needs to be within for the enemy to start chasing the player
+        #checks if player is within range of goblin, if true it sets goblin path towards player
+        if(self.sqrDistToPlayer(self, plr) < range*range): # range is a variable u declare at the start, represents the pixel range that the player needs to be within for the enemy to start chasing the player
             self.gotoX = plr.x
             self.gotoY = plr.y
-            currentSprite = 1;
         else:
             self.gotoX = self.pathToPoints[0].x
             self.gotoY = self.pathToPoints[0].y
             #remove path to points [0] once they have reached it
 
     def attackBehavior(self):
-        pass;
+    
+        attackType = random.randint(1, 100) #random number between 1 - 100 determines attack type
+        if(self.sqrDistToPlayer(self, plr) < 4*4):
+            if(attackType <= 80):
+                currentSprite = 0 #80% chance the goblin will walk
+            elif(attackType <= 15):
+                currentSprite = 1 #15% chance the goblin will try a basic attack(i think biting would be very funny but that can be for later)
+            elif(attackType <= 5):
+                currentSprite = 2 #5% chance the goblin will dash
+        else:
+            self.path(self)
 
     def draw(self, surf):
         super().draw()
@@ -43,3 +56,5 @@ class Goblin(enemy.Enemy):
 
     def update(self):
         super().update()
+        if(self.attacking): 
+            self.attackBehavior(self)
